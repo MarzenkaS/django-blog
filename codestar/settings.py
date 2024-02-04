9a5f4cd168a59d8216813256c2da0881321e8358
MarzenkaS <>
2024-02-04 21:41:09 +0000

    Update

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('maczenta_loca_maczenta_loca')
+SECRET_KEY = os.environ.get('django-insecure-&m%gu-ez==#b@$+_t99xi_wgv()$4vr7#$-og^=x4rdhx6lh6j')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-marzenkas-django-blog-8yd0zblr50.us2.codeanyapp.com',
