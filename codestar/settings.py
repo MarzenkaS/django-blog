71a1adfe9760ad03bad071d2d6e4f8d2909ff1cc
MarzenkaS <>
2024-02-06 21:32:24 +0000

    Create Comment Model

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "8000-marzenkas-django-blog-8yd0zblr50.us2.codeanyapp.com",
