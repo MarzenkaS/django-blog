783e62c64202a89a41716c67905e19899f6019a8
Marzena Skórska <maczenta@gmail.com>
2024-02-22 20:31:04 +0000

    Upload a foto to about me page

--- about/admin.py
+++ about/admin.py
@@ -5,10 +5,11 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(About)
 class AboutAdmin(SummernoteModelAdmin):
+
      """
-    Adds rich-text editing of content in admin
-    """
-    summernote_fields = ('content',)
+     Adds rich-text editing of content in admin
+     """
+     summernote_fields = ('content',)
 
 # Note: admin.ModelAdmin is the standard way of registering
 #       our model with the admin panel. We do it differently
@@ -19,7 +20,8 @@ class AboutAdmin(SummernoteModelAdmin):
 
 @admin.register(CollaborateRequest)
 class CollaborateRequestAdmin(admin.ModelAdmin):
+
      """
-    Lists message and read fields for display in admin
-    """
-    list_display = ('message', 'read',)
+     Lists message and read fields for display in admin
+     """
+     list_display = ('message', 'read',)   
